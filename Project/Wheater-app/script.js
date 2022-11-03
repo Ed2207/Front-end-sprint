4e82ae8f0c04c338332f0bc1fa4b9b115dee3595
Ed <polet.edouard@gmail.com>
2022-11-03 11:10:50 +0100

    'Project'

---
+++ Project/Wheater-app/script.js
@@ -0,0 +1,42 @@
+
+const button = document.querySelector('button');
+const apiKey = "73753b8fc6msh60257a98da68f59p1e28a5jsnd1bcac5b9e1"
+const ul = document.querySelector('ul');
+
+button.addEventListener ('click', (event) => {
+        // console.log(event)
+        let test = document.createElement('li');
+         let contentTest = document.createTextNode ('Hello');
+         test.appendChild(contentTest)
+        }
+
+    // ul.appendChild(test)
+
+
+
+
+    // const city = document.querySelector('input').value;
+    // const list = document.querySelector('ul');
+
+)
+// button.addEventListener ('click', () => {
+
+// }
+
+// )
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '73753b8fc6msh60257a98da68f59p1e28a5jsnd1bcac5b9e15',
+// 		'X-RapidAPI-Host': 'national-weather-service.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://national-weather-service.p.rapidapi.com/zones/brussels/%7BzoneId%7D/forecast', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+
